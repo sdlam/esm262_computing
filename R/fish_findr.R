ac4728e90d8a35f6a79bf5517dea7b308b4c345b
Sarah Lam <sdlam@bren.ucsb.edu>
2023-03-07 23:32:22 -0800

    fish assignment

---
+++ R/fish_findr.R
@@ -0,0 +1,23 @@
+#' Fish Finder Function 
+#'
+#' This function finds the most common fish, the rarest fish, and the total number of fish
+#' @param data as a vector of all the fish from a days catch 
+
+# function definition
+fish_findr = function(data) {
+  
+  fish_species = as.factor(data)
+  
+  # number of fish
+  nfish = length(data)
+  
+  # which is the most common fish
+  common = names(which.max(summary(fish_species)))
+  
+  # which is the rarest common fish
+  rarest = names(which.min(summary(fish_species)))
+  
+  
+  # output from function
+  return(list(num=nfish, common=common, rarest=rarest))
+}
